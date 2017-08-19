2b742dade4c536ef3c05d986f6c570a477c2b644
Gabriele Petronella <gabriele@buildo.io>
2017-08-19 12:46:12 +0200

    Show progress in case formatting takes a while.

--- src/extension.ts
+++ src/extension.ts
@@ -22,33 +22,40 @@ function retrieveConfig(): string | undefined {
   }
 }
 
-function formatDocument(document: vscode.TextDocument, isSbt: Boolean, range: vscode.Range | undefined, output: vscode.OutputChannel): vscode.TextEdit[] {
-  try {
-    const scalafmtConfig = retrieveConfig(); 
-    const formattedFile = format(
-      document.getText(),
-      isSbt,
-      scalafmtConfig,
-      range ? [{ start: range.start.line, end: range.end.line + 1 }] : undefined
-    );
-    output.clear();
-    return [vscode.TextEdit.replace(fullDocumentRange(document), formattedFile)];
-  } catch (e) {
-    console.error(e);
-    output.clear();
-    output.appendLine('An error occurred while formatting this file with Scalafmt:\n');
-    output.append(e);
-    output.appendLine('\nIf this is unexpected, consider filing an issue at https://github.com/gabro/vscode-scalafmt/issues/new');
-    vscode.window.showErrorMessage(
-      `Error while formatting document: ${e}`,
-      'Show full output'
-    ).then(selection => {
-      switch(selection) {
-        case 'Show full output': output.show(true);
-      }
-    });
-    return [];
-  }
+async function formatDocument(document: vscode.TextDocument, isSbt: Boolean, range: vscode.Range | undefined, output: vscode.OutputChannel): Promise<vscode.TextEdit[]> {
+
+  return vscode.window.withProgress({
+    location: vscode.ProgressLocation.Window,
+    title: "Formatting with Scalafmt..."
+  }, progress => new Promise<vscode.TextEdit[]>((resolve, reject) => {
+    try {
+      const scalafmtConfig = retrieveConfig(); 
+      const formattedFile = format(
+        document.getText(),
+        isSbt,
+        scalafmtConfig,
+        range ? [{ start: range.start.line, end: range.end.line + 1 }] : undefined
+      );
+      output.clear();
+      resolve([vscode.TextEdit.replace(fullDocumentRange(document), formattedFile)]);
+    } catch (e) {
+      console.error(e);
+      output.clear();
+      output.appendLine('An error occurred while formatting this file with Scalafmt:\n');
+      output.append(e);
+      output.appendLine('\nIf this is unexpected, consider filing an issue at https://github.com/gabro/vscode-scalafmt/issues/new');
+      vscode.window.showErrorMessage(
+        `Error while formatting document: ${e}`,
+        'Show full output'
+      ).then(selection => {
+        switch(selection) {
+          case 'Show full output': output.show(true);
+        }
+      });
+      reject([]);
+    }
+  }));
+  
 }
 
 function createScalafmtStatusBarItem(): vscode.StatusBarItem {
@@ -81,7 +88,7 @@ export function activate(context: vscode.ExtensionContext) {
 
   const formattingEditProviders = supportedLanguages.map(languageId => {
     return vscode.languages.registerDocumentFormattingEditProvider(languageId, {
-      provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
+      async provideDocumentFormattingEdits(document: vscode.TextDocument): Promise<vscode.TextEdit[]> {
         return formatDocument(document, languageId === sbtLanguageId, undefined, scalafmtOutput);
       }
     });
@@ -89,7 +96,7 @@ export function activate(context: vscode.ExtensionContext) {
 
   const rangeFormattingEditProviders = supportedLanguages.map(languageId => {
     return vscode.languages.registerDocumentRangeFormattingEditProvider(scalaLanguageId, {
-      provideDocumentRangeFormattingEdits(document: vscode.TextDocument, range: vscode.Range): vscode.TextEdit[] {
+      async provideDocumentRangeFormattingEdits(document: vscode.TextDocument, range: vscode.Range): Promise<vscode.TextEdit[]> {
         return formatDocument(document, languageId === sbtLanguageId, range, scalafmtOutput);
       }
     });
